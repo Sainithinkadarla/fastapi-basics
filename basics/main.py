7f650497c3728bbf8c9ad358e308a751b8a12e01
Sainithinkadarla <sainithin2004@gmail.com>
2025-07-06 17:37:14 +0530

    added post method

--- basics/main.py
+++ basics/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+#pip install fastapi uvicorn
+class User(BaseModel):
+    user_id: int
+    grade: float
+    graduate: bool = None
 
 test = FastAPI()
 
 @test.get("/")
 def read_root():
-    return {"message": "Welcome to FastAPI!!!"}
+    return {"message": "Welcome to FastAPI updated!!!"}
+
+#Let's write other endpoints
+
+@test.get('/users/{user_id}')
+def get_user(user_id: int, q: str=None):
+    return {"user id": user_id, "query": q}
+
+@test.post("/users/")
+def create_user(user: User):
+    return {"User ID": user.user_id, "Grade": user.grade, "Graduated": "Yes" if user.graduate else "No"}
+    
