234b5937ead667f5d5b0f114ac16860a3199776b
Sainithinkadarla <sainithin2004@gmail.com>
2025-07-07 23:59:37 +0530

    learnt broadcasting

---
+++ websockets/broadcast.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, WebSocket
+
+app = FastAPI()
+
+active_connections = []
+
+@app.websocket("/broadcast")
+async def broadcast_all(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.append(websocket)
+    
+    try:
+        while True:
+            msg = await websocket.receive_text()
+            for connection in active_connections:
+                if connection != websocket:
+                    await connection.send_text(f"Broadcast message: {msg}")
+    except Exception as e:
+        print(f"Error : {e}")
+        # active_connections.remove(websocket)
+    finally:
+        if websocket in active_connections:
+            active_connections.remove(websocket)
+        print("closing")
+        await websocket.close()
