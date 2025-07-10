bcb22b403a34731b4524e07b8ffefb7713c31477
Sainithinkadarla <sainithin2004@gmail.com>
2025-07-10 23:50:33 +0530

    Added authent

---
+++ security/jwt/app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException, status, Depends
+from security import create_access_token, verify_token
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordRequestForm
+#!pip install passlib, python-dotenv, pyjwt, python-multipart
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    password: str
+
+@app.post("/token")
+async def login(formdata: OAuth2PasswordRequestForm = Depends()):
+    user = User(username=formdata.username, password=formdata.password)
+
+    if user.username == "admin" and user.password == "pass":
+        access_token = create_access_token({"sub":user.username})
+        return {"access_token": access_token, "token_type": "Bearer"}
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password",
+        headers={"WWW-Authentication": "Bearer"}
+    )
+
+@app.get("/protected")
+async def protected(token: str = Depends(verify_token)):
+    return {"Message": "This is protected endpoint"}
---
+++ security/jwt/app/security.py
@@ -0,0 +1,39 @@
+#!pip install passlib, python-dotenv, pyjwt, python-multipart
+import jwt
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+import os
+
+load_dotenv()
+
+KEY = os.getenv("SECRET")
+ALGORITHM = "HS256"
+EXPIRE_TIME = 30
+
+
+oauthscheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+
+def create_access_token(data:dict, expire_delta:timedelta=None):
+    if expire_delta is None:
+        expire_delta = timedelta(minutes=EXPIRE_TIME)
+
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expire_delta
+    to_encode.update({"expire":expire.isoformat()})
+    encoded = jwt.encode(to_encode, KEY, algorithm=ALGORITHM)
+    return encoded
+
+def verify_token(token:str):
+    try:
+        decoded = jwt.decode(token, KEY, algorithms=[ALGORITHM])
+        return decoded
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate":"Bearer"}
+        )
