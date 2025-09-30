702df2125a63ace30fc06ef2a60a64324178edbe
Sainithinkadarla <my.life496s@gmail.com>
2025-09-30 05:24:00 +0000

    path parameters with other options

--- book/chapter_3/main.py
+++ book/chapter_3/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from enum import Enum
+
+class UserType(str, Enum):
+    HR = 'hr'
+    STANDARD = "standard"
+    ADMIN = "admin"
 
 app = FastAPI()
 
@@ -9,3 +15,25 @@ async def home():
 @app.get("/users/{id}")
 async def get_user_id(id: int):
     return {"Response": f"{id} is retrieved" }
+
+@app.get("/users/{id}/{type}")
+async def get_user_type_id(id: int, type: str):
+    return {"id": id,
+            "type": type }
+
+@app.get("/users-enum/{id}/{type}")
+async def get_user_type_id_with_enum(id: int, type: UserType):
+    return {"id": id,
+            "type": type }
+
+@app.get("/users-path/{id}")
+async def get_user_type_id_with_path(id: int = Path(..., le=1)):
+    return {"id": id}
+
+@app.get("/license-plates/{license}")
+async def get_license(license: str = Path(..., min_length=9, max_length=9)):
+    return {"license": license}
+
+@app.get("/license-plates-regex/{license}")
+async def get_license(license: str = Path(..., regex=r"^\w{2}-\d{3}-\w{2}$")):
+    return {"license": license}
