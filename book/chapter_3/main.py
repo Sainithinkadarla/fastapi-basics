df90c676595e7b5cf84c5b74da1b1ff717896db6
Sainithinkadarla <my.life496s@gmail.com>
2025-09-30 09:53:06 +0000

    query parameters and request body

--- book/chapter_3/main.py
+++ book/chapter_3/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query, Body
 from enum import Enum
+from pydantic import BaseModel
 
 class UserType(str, Enum):
     HR = 'hr'
@@ -41,3 +42,52 @@ async def get_license(license: str = Path(..., regex=r"^\w{2}-\d{3}-\w{2}$")):
 
 
 # Query parameters
+@app.get("/users")
+async def get_user(page: int = 1, size: int = 10):
+    return {"page" : page, "size" : size}
+
+## required query parameter
+
+class UsersType(str, Enum):
+    BEST = "best"
+    AVERAGE = "average"
+
+@app.get("/type")
+async def get_type(type: UsersType):
+    return {"type" : type}
+
+## Other validation options
+# Import Query function from fastapi package
+@app.get("/users_ad")
+async def get_users(page: int = Query(1, ge=0), size: int = Query(10, le=100)):
+    return {"page" : page, "size" : size}
+
+# request body
+# Import Body function from fastapi package
+@app.post("/users_body")
+async def  get_user_body(name: str = Body(...), age: int = Body()):
+    return {"Name" : name, "Age" : age}
+
+## Pydantic models
+# import BaseModel class from Pydantic package
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.post("/user-pyd")
+async def get_user_post(user: User):
+    return {"Name" : user.name, "Age" : user.age}
+
+
+## Multiple objects
+class Company(BaseModel):
+    c_name: str
+    c_location: str
+
+@app.post("/users/create")
+async def create_user(user: User, company: Company):
+    return {"user" : user, "company" : company}
+
+@app.post("/task")
+async def create_task(user: User, priority: int = Body(..., ge=1, le=3)):
+    return {"user" : user, "priority" : priority}
