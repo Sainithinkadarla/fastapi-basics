3507e88d8fc1c9376bbefd77012b90f1ae860d26
Sainithinkadarla <my.life496s@gmail.com>
2025-10-09 12:24:55 +0000

    getting and filtering objects

--- book/chapter_6/project/app.py
+++ book/chapter_6/project/app.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, status, HTTPException, Depends
+from fastapi import FastAPI, status, HTTPException, Depends, Query
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 from project.schemas import PostCreate, PostPartialUpdate, PostRead
 from project.database import create_all_tables
@@ -11,6 +12,25 @@ async def lifespan(app:FastAPI):
     await create_all_tables()
     yield       
 
+async def pagination(skip: int = Query(0, ge=0), 
+                     limit: int = Query(10, ge=10)):
+    capped_limit = min(100, limit)
+    return (skip, capped_limit)
+
+async def get_post_or_404(id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(Post).where(Post.id == id)
+    result = await session.execute(query)
+    post = result.scalar_one_or_none()
+    print(post.id)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    return post
+
+
+
+
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/posts", response_model=PostRead, status_code=status.HTTP_201_CREATED)
@@ -20,6 +40,16 @@ async def create_post(post_create: PostCreate, session: AsyncSession = Depends(g
     await session.commit()
     return post
 
-@app.get("/posts")
-async def get_posts():
-    pass
+@app.get("/posts", response_model=list[PostRead])
+async def get_posts(pagination: tuple[int, int] = Depends(pagination), 
+                    session: AsyncSession = Depends(get_async_session)):
+    skip, limit = pagination
+    #Import select function from sqlalchemy package
+    query = select(Post).offset(skip).limit(limit)
+    results = await session.execute(query)
+    return results.scalars().all()
+
+@app.get("/posts/{id}", response_model=PostRead)
+async def get_post(post: Post = Depends(get_post_or_404)):
+    return post
+    
