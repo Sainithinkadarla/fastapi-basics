ad7fc6a6a0581d46f6a6bc3ede0eb48b9f946b68
Sainithinkadarla <my.life496s@gmail.com>
2025-10-09 07:55:40 +0000

    ORM model

---
+++ book/chapter_6/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(Integer, autoincrement = True, primary_key = True)
+    publication_date: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
+    title = Mapped[str] = mapped_column(String(355), nullable=False)
+    content = Mapped[str] = mapped_column(Text, nullable=False)
