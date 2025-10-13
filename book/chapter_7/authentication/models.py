156e8b82513b6ae3dc42f24a025c6ca78fcf3187
Sainithinkadarla <my.life496s@gmail.com>
2025-10-13 13:55:49 +0000

    models

---
+++ book/chapter_7/authentication/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    id: Mapped[int] = mapped_column(Integer, autoincrement=True, primary_key=True)
+    email: Mapped[str] = mapped_column(String(1024), nullable=False, unique=True)
+    passwd: Mapped[str] = mapped_column(String(1024), nullable=False)
