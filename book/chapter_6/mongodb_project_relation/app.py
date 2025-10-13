0ddcef8dd1b17ee817a7a38255c50f116fad61ff
Sainithinkadarla <my.life496s@gmail.com>
2025-10-13 08:00:18 +0000

    updated with existing code

---
+++ book/chapter_6/mongodb_project_relation/app.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, Depends, status, HTTPException, Query
+from database import get_db, AsyncIOMotorDatabase
+from bson import ObjectId, errors
+
+from models import *
+
+app = FastAPI()
+
+async def get_object_id(id: str) -> ObjectId:
+    try:
+        return ObjectId(id)
+    except (errors.InvalidId, TypeError):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+async def get_post_or_404(id: ObjectId = Depends(get_object_id),
+                           database: AsyncIOMotorDatabase = Depends(get_db)):
+    
+    raw_post = await database["posts"].find_one({"_id": id})
+    if raw_post is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    return Post(**raw_post)
+
+async def pagination(skip: int = Query(0, ge=0),
+                     limit: int = Query(10, ge=0)
+                    ):
+    capped_limit = min(15, limit)
+    return (skip, capped_limit)
+
+
+@app.post("/posts", response_model=Post, status_code=status.HTTP_201_CREATED)
+async def create_post(post_create: PostCreate,
+                      database: AsyncIOMotorDatabase = Depends(get_db)):
+    post = Post(**post_create.model_dump())
+
+    upload_post = await database["posts"].insert_one(post.model_dump())
+    print(upload_post.inserted_id, type(upload_post.inserted_id))
+    result_post = await get_post_or_404(upload_post.inserted_id, database)
+    return result_post
+
+# Getting all documents
+@app.get("/posts", response_model=list[Post])
+async def get_all(database: AsyncIOMotorDatabase = Depends(get_db),                   
+                      pagination: tuple[int, int] = Depends(pagination)):
+    skip, limit = pagination
+    query = database['posts'].find({}, skip = skip, limit = limit)
+    results = [Post(**raw_post) async for raw_post in query]
+    return results
+
+# Getting a single object
+@app.get("/posts/{id}", response_model=Post)
+async def get_post(post: Post = Depends(get_post_or_404)):
+    return post
+
+# Updating the document
+@app.patch("/posts/{id}")
+async def update_post(post_update: PostPartialUpdate,
+                      post: Post = Depends(get_post_or_404),
+                      database: AsyncIOMotorDatabase = Depends(get_db)):
+    
+    update_fields = post_update.model_dump(exclude_unset=True)
+    
+    await database["posts"].update_one({"_id": post.id}, {"$set": update_fields})
+
+    post = await get_post_or_404(post.id, database)
+    return post
+
+# Deleting the document
+@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
+async def delete_post(post: Post = Depends(get_post_or_404),
+                      database: AsyncIOMotorDatabase = Depends(get_db)):
+    await database["posts"].delete_one({"_id": post.id})
---
+++ book/chapter_6/mongodb_project_relation/models.py
@@ -0,0 +1,56 @@
+from bson import ObjectId
+from datetime import datetime
+from typing import Any
+
+from pydantic import Field, BaseModel, ConfigDict
+from pydantic_core import core_schema, PydanticCustomError
+from pydantic.json_schema import JsonSchemaValue
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def validate_object_id(cls, value: Any) -> ObjectId:
+        if isinstance(value, ObjectId):
+            return value
+        if isinstance(value, str):
+            if not ObjectId.is_valid(value):
+                raise PydanticCustomError('InvalidObjectId', 'Invalid ObjectId')
+            return ObjectId(value)
+        raise PydanticCustomError('InvalidObjectId', 'Invalid ObjectId')
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.no_info_before_validator_function(
+            cls.validate_object_id,
+            core_schema.is_instance_schema(ObjectId)
+        )
+    
+    @classmethod
+    def __get_pydantic_json_schema__(cls, core_schema: Any, handler: Any) -> JsonSchemaValue:
+        json_schema = handler(core_schema)
+        json_schema.update(type="string", format="ObjectId")
+        return json_schema
+
+class MongoBaseModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    model_config = ConfigDict(
+        populate_by_name=True, 
+        arbitrary_types_allowed=True,
+        json_encoders = {ObjectId: str}
+    )
+
+class PostBase(MongoBaseModel):
+    title: str
+    content: str
+    publication_date: datetime = Field(default_factory=datetime.now)
+
+class Post(PostBase):
+    pass
+
+class PostCreate(PostBase):
+    pass
+
+class PostPartialUpdate(BaseModel):
+    title : str | None = None
+    content : str | None = None
