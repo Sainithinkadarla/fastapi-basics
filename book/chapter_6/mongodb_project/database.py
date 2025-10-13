701882f74e0916ac7395e0b8a144059e2edacb43
Sainithinkadarla <my.life496s@gmail.com>
2025-10-13 07:09:25 +0000

    editted connection string ip address

--- book/chapter_6/mongodb_project/database.py
+++ book/chapter_6/mongodb_project/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
-motor_client = AsyncIOMotorClient("mongodb://192.168.29.94:27017")
+motor_client = AsyncIOMotorClient("mongodb://172.17.0.3:27017")
 
 database = motor_client['sample']
 
