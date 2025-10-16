531a0e4a53fbfda810a07805eea96866bf99beba
Sainithinkadarla <my.life496s@gmail.com>
2025-10-16 13:43:29 +0000

    created a broadcast endpoint

---
+++ book/chapter_8/broadcast.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, WebSocket, status, WebSocketException
+import broadcaster
+import asyncio
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from starlette.websockets import WebSocketDisconnect
+
+@asynccontextmanager
+async def lifespan(websocket:WebSocket):
+    await broadcast.connect()
+    yield 
+    await broadcast.disconnect()
+
+broadcast = broadcaster.Broadcast("redis://localhost:6379")
+channel = "chat"
+
+class MessageEvent(BaseModel):
+    username: str 
+    message: str
+
+
+
+async def receive_message(websocket: WebSocket, username: str):
+    async with broadcast.subscribe(channel=channel) as subscriber:
+        async for event in subscriber:
+            message_event = MessageEvent.parse_raw(event.message)
+            if message_event.username != username:
+                await websocket.send_json(message_event.model_dump())
+
+async def send_message(websocket: WebSocket, username: str):
+    data = await websocket.receive_text()
+    event = MessageEvent(username=username, message=data)
+    await broadcast.publish(channel, message=event.model_dump_json())
+
+app = FastAPI(lifespan=lifespan)
+
+@app.websocket("/ws")
+async def endpoint(username: str, websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            receive_task = asyncio.create_task(receive_message())
+            send_task = asyncio.create_task(send_message())
+            done, pending = await asyncio.wait({receive_task, send_task},
+                                               return_when=asyncio.FIRST_COMPLETED)
+            for t in pending:
+                t.cancel()
+            for t in done:
+                t.result()
+    except WebSocketDisconnect:
+        pass
